27e140800bb8bd5046b1a51dc4df6634bc841eba
mrunelov <martin.runelov@gmail.com>
2014-04-08 16:18:39 +0200

    commented

--- digenv.c
+++ digenv.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv, char **envp) {
+
+    // no parameters corresponds to "printenv | sort | less".
+    // the pager used (default: less) is set by the env. var. PAGER.
+    // We can read env. vars. with "getenv(const char* name)"
+
 	int i;
 	for(i = 0; envp[i] != NULL; ++i) {
 		printf("%2d:%s\n", i, envp[i]);
