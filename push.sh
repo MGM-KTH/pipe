cb33d63f292a2706dc8d8dbb8a3e0e9eaf78fba1
mrunelov <martin.runelov@gmail.com>
2014-04-08 17:08:35 +0200

    renamed commit.sh to push.sh. it now pushes

--- push.sh
+++ push.sh
@@ -1,9 +1,10 @@
 git status
 echo    # (optional) move to a new line
-read -p "Commit digenv.c with the commit message '$1'? " -n 1 -r
+read -p "Push digenv.c with the commit message '$1'? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     git add digenv.c
     git commit -m '$1'
+    git push
 fi
