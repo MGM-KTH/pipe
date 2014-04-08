1af158080e51b2a009adb6648742eba2611ecb9d
mrunelov <martin.runelov@gmail.com>
2014-04-08 16:46:07 +0200

    commit script

---
+++ commit.sh
@@ -0,0 +1,8 @@
+git status
+read -p "Commit digenv.c with the commit message '$1'? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    git add digenv.c
+    git commit -m '$1'
+fi
