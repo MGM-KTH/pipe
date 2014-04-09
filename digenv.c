f6d972f3b92ed097bc4d2a2d3e88f4fcddfffef2
Gustaf Lindstedt <gustaflindstedt@gmail.com>
2014-04-09 11:35:08 +0200

    added register function for signal handler

--- digenv.c
+++ digenv.c
@@ -7,6 +7,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+void register_sighandler( int signal_code, void (*handler) (int sig) )  {
+    int retval;
+
+    struct sigaction signal_parameters;
+
+    signal_parameters.sa_handler = handler;
+    sigemptyset(&signal_parameters.sa_mask);
+    signal_parameters.sa_flags = 0;
+
+    retval = sigaction(signal_code, &signal_parameters, (void *) 0);
+
+    if(-1 == retval) {
+        perror("sigaction() failed");
+        exit(1);
+    }
+}
 
 int main(int argc, char **argv, char **envp) {
 
